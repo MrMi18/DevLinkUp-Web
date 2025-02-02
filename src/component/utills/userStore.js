f6d983fea761afa8aaa662cd1ac30fe84440dda2
Mr Mi <is8388508@gmail.com>
2025-02-02 21:24:48 +0530

    feed added to redux store

--- src/component/utills/userStore.js
+++ src/component/utills/userStore.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  userReducer from "./userSlice";
+import feedReducer from "./feedSlice";
 
 const userStore = configureStore({
     reducer:{
         user:userReducer,
+        feed:feedReducer,
+        
     },
 })
 export default userStore;
